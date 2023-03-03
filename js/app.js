dc4d5dbe4c2a6d5b41b13a539693da4a2005b297
Mainul7669 <mainul7669@gmail.com>
2023-03-04 00:48:23 +0600

    modal added

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ const displayHub = (universe, dataLimit) => {
         <ol>
         <li>${tool.features[0]}</li>
         <li>${tool.features[1]}</li>
-        <li>${tool.features[2]}</li>
+        <li>${tool.features[2] ? tool.features[2]: "No data found"}</li>
         </ol>
         </div>
         <hr>
@@ -45,7 +45,7 @@ const displayHub = (universe, dataLimit) => {
         </div>
 
         <div>
-        <button class="btn btn-info">
+        <button onclick="loadApiDetails('${tool.id}')" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#apiDetailModal">
         <i class="fa-solid fa-arrow-right"></i>
         </button>
         </div>
@@ -78,3 +78,85 @@ document.getElementById('btn-show-all').addEventListener('click', function () {
 
 
  loadAiUniverse(6);
+
+
+ const loadApiDetails = async id =>{
+    const url =`https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayApiDetails(data.data);
+}
+
+const displayApiDetails = tool =>{
+    console.log(tool);
+    const modalDetails = document.getElementById('api-details');
+
+    modalDetails.innerHTML = `
+<div class="container d-flex justify-content-between"> 
+
+<div class="border border-danger rounded-5 p-3">
+    <h5>${tool.description}</h5>
+
+    <div class="d-flex justify-content-between  mt-5 mb-5">
+
+    <div class="border rounded-3 p-4 text-success">
+    ${tool.pricing[0].price}
+    <br>
+    ${tool.pricing[0].plan}
+    </div>
+
+    <div  class="border rounded-3 p-4 text-warning">
+    ${tool.pricing[1].price}
+    <br>
+    ${tool.pricing[1].plan}
+    </div>
+
+    <div  class="border rounded-3 p-4 text-danger">
+    ${tool.pricing[2].price}
+    <br>
+    ${tool.pricing[2].plan}
+    </div>
+
+    </div>
+
+    <div class="d-flex justify-content-between">
+    
+    <div>
+    <h5>Features</h5>
+    <li>${tool.features[1].feature_name}</li>
+    <li>${tool.features[2].feature_name}</li>
+    <li>${tool.features[3].feature_name}</li>
+    </div>
+
+    <div>
+    <h5>Integrations</h5>
+    <li>${tool.integrations[0]}</li>
+    <li>${tool.integrations[1]}</li>
+    <li>${tool.integrations[2]}</li>
+    </div>
+
+    </div>
+    </div>
+
+
+    <div class = "border border-light-subtle rounded-5 p-3 ">
+
+
+
+    <img src="${tool.image_link[0]}" class = "card-img-top rounded-3">
+
+    <button id="accuracy" class=" btn btn-danger text-center pe-5 ps-5 position-absolute">${tool.accuracy.score ? tool.accuracy.score: "no data found" }</button>
+
+
+
+    <h4>${tool.input_output_examples[0].input ? tool.input_output_examples[0].input: "No! Not Yet! Take a break!!!"}</h4>
+
+    <p>${tool.input_output_examples[0].output}</p>
+    </div>
+
+
+    </div>
+    
+
+    `
+}
