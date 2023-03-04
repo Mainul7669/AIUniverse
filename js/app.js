24fa3c6b42acbbbd4ce4a324eb1a8d85e20a7b9b
Mainul7669 <mainul7669@gmail.com>
2023-03-04 17:45:21 +0600

    accuracy placed update

--- js/app.js
+++ js/app.js
@@ -7,7 +7,55 @@ const loadAiUniverse = async(dataLimit) => {
 
 }
 
+const displayHub = (universe, dataLimit) => {
+    const hubContainer = document.getElementById('hub-container');
+    hubContainer.textContent= '';
 
+ // display 6 card only 
+ const showAll = document.getElementById('show-all');
+ if(dataLimit && universe.length > 6) {
+     universe = universe.slice(0, 6);
+ }
+ else{
+     showAll.classList.add('d-none');
+ }
+
+
+
+// display all card
+   
+        <div class ="card p-4 rounded-4">
+        <img src="${tool.image}" class = "card-img-top rounded-3">
+        <div class="card-body">
+        <h4 class= "card-title">Features</h4>
+        <ol>
+        <li>${tool.features[0]}</li>
+        <li>${tool.features[1]}</li>
+        <li>${tool.features[2] ? tool.features[2]: "No data found"}</li>
+        </ol>
+        </div>
+        <hr>
+        <div class="d-flex justify-content-between align-items-center">
+        <div>
+        <h3 class= "card-title">${tool.name}</h3>
+        <i class="fa-solid fa-calendar-days"></i> ${tool.published_in}
+        </div>
+
+        <div>
+        <button onclick="loadApiDetails('${tool.id}')" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#apiDetailModal">
+        <i class="fa-solid fa-arrow-right"></i>
+        </button>
+        </div>
+        </div>
+
+        </div>
+        </div>
+        `;
+        hubContainer.appendChild(toolDiv);
+    });
+        // stop spinner or loader
+        toggleSpinner(false);
+}
 
 
 const toggleSpinner = isLoading => {
